bfd4955013041afceb16d798754a7b79daa3ea02
Kenneth Bergquist <bergquist.kenneth@gmail.com>
2016-04-05 13:18:11 -0400

    update region

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,7 +94,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  
+
   config.paperclip_defaults = {
       :storage => :s3,
       :s3_region => ENV['AWS_REGION'],
